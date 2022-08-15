444af4a5ce2b9c3b7255feed076ae4772d92e6f4
tianhaozhang <zhangtianhao@tezign.com>
2022-08-15 17:09:14 +0800

    init

--- lark/__init__.py
+++ lark/__init__.py
@@ -1,3 +1,7 @@
 base_url = "https://open.feishu.cn/open-apis"
 url_get_tenant_access_token = "/auth/v3/tenant_access_token/internal"
-url_get_records = "/bitable/v1/apps/:app_token/tables/:table_id/records"
+url_fetch_records = "/bitable/v1/apps/:app_token/tables/:table_id/records"
+url_update_records = "/bitable/v1/apps/:app_token/tables/:table_id/records/batch_update"
+url_create_records = "/bitable/v1/apps/:app_token/tables/:table_id/records/batch_create"
+url_delete_records = "/bitable/v1/apps/:app_token/tables/:table_id/records/batch_delete"
+url_record = "/bitable/v1/apps/:app_token/tables/:table_id/records/:record_id"
--- vo/lark/LarkBitableRecordItem.py
+++ vo/lark/LarkBitableRecordItem.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class LarkBitableRecordItem(BaseModel):
-    id: str
+    id: Optional[str]
     record_id: str
     fields: dict
